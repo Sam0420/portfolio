e69ccde5c2d8b13de9b4ce99b6d07acf79b584bd
sam0420 <samhashimi10@gmail.com>
2025-04-08 20:16:24 +0200

    added better project card layout for a more slicker look

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,30 +1,32 @@
-import styles from './ProjectCard.module.css'
+import styles from './ProjectCard.module.css';
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 
-interface ProjectCardProps  {
-    image : string; 
-    title : string; 
-    github : string; 
-    demo : string; 
+interface ProjectCardProps {
+  image: string;
+  title: string;
+  description: string;
+  github: string;
+  demo: string;
 }
 
-export const ProjectCard = ({image, title, github, demo} : ProjectCardProps) => {
-    return (
-        <div className={styles.projectCard}>
-            <img src={image} alt={title} className={styles.projectImage} />
-            <div className={styles.projectContent}>
-                <h3> {title} </h3>
-                <div className={styles.projectLinks}>
-                    <a href={github} target="_blank" rel="noopener noreferrer">
-                        <FaGithub/>
-                    </a>
-
-                    <a href={demo} target="_blank" rel="noopener noreferrer">
-                        <FaExternalLinkAlt/>
-                    </a>
-                
-                </div>
-            </div>
+export const ProjectCard = ({ image, title, description, github, demo }: ProjectCardProps) => {
+  return (
+    <div className={styles.projectCard}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={title} className={styles.projectImage} />
+        <div className="overlay">
+          <a href={github} target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaGithub />
+          </a>
+          <a href={demo} target="_blank" rel="noopener noreferrer" className={styles.icon}>
+            <FaExternalLinkAlt />
+          </a>
         </div>
-    );
-}
+      </div>
+      <div className={styles.projectInfo}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
